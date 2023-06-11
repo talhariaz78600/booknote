23d774bd9d295a9b4264a877c89fdb2c8b628a64
Talha Riaz <muhammadtalha1005@gmail.com>
2023-06-11 18:10:28 +0500

    ver

--- index.js
+++ index.js
@@ -9,16 +9,7 @@ app.use(cors());
 app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
-// ////////////////severing the fornted////////////
-// app.use(express.static(path.join(__dirname,'./inotebook/build')))
-// app.get("*",(req,res)=>{
-//   res.sendFile(
-//     path.join(__dirname,"./inotebook/build/index.html"),
-//     function(err){
-//       res.status(500).send(err);
-//     }
-//   )
-// })
+
 app.listen(port, () => {
   console.log(`inotebook app listening on port ${port}`)
 })
